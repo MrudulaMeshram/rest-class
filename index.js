f236b1813a3b3e99ae79a116cc908bf30c0506b6
Mrudula Meshram <mrudulameshram138@gmail.com>
2026-04-01 17:06:51 +0530

    Add edit functionality and fix post routes

--- index.js
+++ index.js
@@ -1,62 +1,94 @@
 const express = require("express");
 const app = express();
-const port =8080;
+const port = 8080;
 const path = require("path");
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
+const methodOverride = require("method-override");
 
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
- // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(express.urlencoded({extended:true}));
-
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"));
-
-app.use(express.static(path.join(__dirname,"public")));
-
- let posts = [
-    {  
-        id:uuidv4(),
-        username:"Mrudla",
-        content:"this is post 1"
-    },
-    {       
-        id:uuidv4(),
-        username:"raj",
-        content:"this is post 2"
-    },
-    {
-        id:uuidv4(),
-        username:"lata",
-        content:"this is post 3"
-    },
+let posts = [
+  {
+    id: uuidv4(),
+    username: "Mrudla",
+    content: "this is post 1",
+  },
+  {
+    id: uuidv4(),
+    username: "raj",
+    content: "this is post 2",
+  },
+  {
+    id: uuidv4(),
+    username: "lata",
+    content: "this is post 3",
+  },
 ];
 
-app.get("/posts",(req,res)=>{
-    res.render("index.ejs",{posts});
+app.get("/posts", (req, res) => {
+  res.render("index", { posts });
 });
 
-app.get("/posts/new",(req,res)=>{
-    res.render("new.ejs");
-});
-app.post("/posts",(req,res)=>{
-    let {username, content} = req.body;
-    let id = uuidv4();
-    posts.push({id, username, content});
-    res.redirect("/posts");
-});
-
-app.get("/posts/:id",(req,res)=>{
-    let {id} = req.params;
-    console.log(id);
-    let  post = posts.find((p)=> id === p.id);
-    res.render("show.ejs",{post});
+app.get("/posts/new", (req, res) => {
+  res.render("new");
 });
 
 
-app.patch("/posts/:id",(req,res)=>{
-    req.send("patch request working");
+app.post("/posts", (req, res) => {
+  let { username, content } = req.body;
+  let id = uuidv4();
+  posts.push({ id, username, content });
+  res.redirect("/posts");
 });
-app.listen(port,()=>{
-    console.log("listening on port :8080");
+
+app.get("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+
+  if (!post) {
+    return res.send("Post not found");
+  }
+
+  res.render("show", { post });
+});
+
+app.get("/posts/:id/edit", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+
+  if (!post) {
+    return res.send("Post not found");
+  }
+
+  res.render("edit", { post });
+});
+
+app.patch("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let newContent = req.body.content;
+
+  let post = posts.find((p) => id === p.id);
+
+  if (!post) {
+    return res.send("Post not found");
+  }
+
+  post.content = newContent;
+  res.redirect(`/posts/${id}`);
+});
+   
+app.delete("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  posts = posts.filter((p) => p.id !== id);
+  res.redirect("/posts");
+});
+
+
+app.listen(port, () => {
+  console.log("listening on port 8080");
 });
